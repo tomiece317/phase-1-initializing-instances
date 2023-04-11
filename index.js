d634e8ad27ff8e295812be62521d9cde8ed3c592
tomiece317 <ceyda666esen@gmail.com>
2023-04-11 20:10:14 +0300

    done

--- index.js
+++ index.js
@@ -1,46 +1,4 @@
 // Write your code here
-/*class Breakfast {
-  constructor(food, drink) {
-    this.food = food;
-    this.drink = drink;
-  }
-}
-
-class Lunch {
-  constructor(salad, soup, drink) {
-    this.salad = salad;
-    this.soup = soup;
-    this.drink = drink;
-  }
-}
-
-class Dinner {
-  constructor(salad, soup, entree, dessert) {
-    this.salad = salad;
-    this.soup = soup;
-    this.entree = entree;
-    this.#dessert = dessert;
-  }
-
-  get dessert() {
-    return this.#dessert;
-  }
-}
-const breakfast = new Breakfast("eggs", "coffee");
-const lunch = new Lunch("Caesar salad", "tomato soup", "iced tea");
-const dinner = new Dinner(
-  "Greek salad",
-  "minestrone soup",
-  "steak",
-  "chocolate cake"
-);
-
-console.log(breakfast);
-console.log(lunch);
-console.log(dinner);
-
-console.log(dinner.dessert);
-*/
 class Breakfast {
   constructor(food, drink) {
     this.food = food;
@@ -65,4 +23,3 @@ class Dinner {
     this.#dessert = dessert;
   }
 }
-
